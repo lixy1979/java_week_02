734242f9de822527d9d80d543ccaf8eb965e8bfa
lixy1979 <120543146+lixy1979@users.noreply.github.com>
2023-01-15 20:37:11 -0500

    first commit being made

---
+++ src/java_week_02/week02_programing.java
@@ -0,0 +1,40 @@
+package java_week_02;
+
+public class week02_programing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		
+		//Print numbers from 0-10:
+		//1. use for loop
+		
+		for(int i = 0; i <= 10; i ++){
+			System.out.println(i);
+			}
+		
+		
+		//2. use while loop
+		
+		int x = 0;
+		while ( x <= 10 ){
+		System.out.println(x);
+		x ++;
+		}
+		
+		
+		//3. use do while loop
+		
+		int i = 0;
+		do {
+		System.out.println(i);
+		i ++;
+		    }while(i <= 10);
+		
+		
+		
+		
+		
+	}
+
+}
